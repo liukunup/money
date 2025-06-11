dc3563ead6fd4e8151f436aa3e8d2f8aa49a8288
刘坤 <liukunup@163.com>
2025-06-11 03:20:48 +0000

    整理目录结构

---
+++ backend/README.md
@@ -0,0 +1,9 @@
+# Money
+
+参考 https://github.com/fastapi/full-stack-fastapi-template/
+
+## 调试
+
+uv pip install .
+
+uv run fastapi dev
--- backend/app/utils/email.py
+++ backend/app/utils/email.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 25))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False').lower() == 'true'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
